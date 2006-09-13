bd286d9b262d909905b1e1fe04aec9cb406f6f01
Lasse Koskela <lasse.koskela@gmail.com>
2006-09-13 20:59:09 +0000

    Added an example for type-based injection and refactored.

--- src/test/org/laughingpanda/beaninject/example/TestExample.java
+++ src/test/org/laughingpanda/beaninject/example/TestExample.java
@@ -1,31 +1,49 @@
 package org.laughingpanda.beaninject.example;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.laughingpanda.beaninject.Inject;
 
 public class TestExample {
 
+    private Target target;
+
     public class Target {
-        private String property;
+        private String field;
 
-        public void setProperty(String value) {
-            this.property = value;
+        public void setPropertyName(String value) {
+            this.field = value;
         }
 
-        public String getProperty() {
-            return property;
+        public String getPropertyName() {
+            return field;
         }
+    }
+
+    @Before
+    public void setUp() {
+        target = new Target();
+        assertNull(target.getPropertyName());
     }
 
     @Test
     public void exampleOfInjectingWithSetter() throws Exception {
-        Target target = new Target();
-        Assert.assertNull(target.getProperty());
-
-        Inject.property("property").of(target).with("value");
-
-        Assert.assertEquals("value", target.getProperty());
+        Inject.property("propertyName").of(target).with("value");
+        assertEquals("value", target.getPropertyName());
     }
 
+    @Test
+    public void exampleOfInjectingDirectlyToField() throws Exception {
+        Inject.field("field").of(target).with("value");
+        assertEquals("value", target.getPropertyName());
+    }
+
+    @Test
+    public void exampleOfTypeBasedInjection() throws Exception {
+        Inject.bean(target).with("value");
+        assertEquals("value", target.getPropertyName());
+    }
 }
